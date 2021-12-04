b92b32912c0907bb01413d6bd6d78adbda888b34
Jéssica Florentino <jdgflorentino@gmail.com>
2021-12-04 11:20:58 -0300

    11 - Crie uma função de Número de Telefone.

--- src/challenges2.js
+++ src/challenges2.js
@@ -6,18 +6,41 @@ function techList(tecnologia, nome) {
 
     for (let i of tecnologia) {
         techname.push({ name: nome, tech: i });
-
     }
     if (tecnologia == 0) {
         return 'Vazio!';
     }
-
     return techname;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(numeros) {
     // seu código aqui
+    let count;
+    if (numeros.length !== 11) {
+        return 'Array com tamanho incorreto.';
+    }
+
+    for (let i = 0; i < numeros.length; i += 1) {
+        for (let j = 0; j < numeros.length; j += 1) {
+            if (numeros[i] === numeros[j]) {
+                count = count + 1;
+            }
+        }
+        if (count >= 3 || numeros[i] < 0 || numeros[i] > 9) {
+            return 'não é possível gerar um número de telefone com esses valores';
+        }
+        count = 0;
+    }
+
+    let ddd = numeros.slice(0, 2);
+    ddd = ddd.join('');
+    let first = numeros.slice(2, 7);
+    first = first.join('');
+    let last = numeros.slice(7);
+    last = last.join('');
+
+    return '(' + ddd + ')' + ' ' + first + '-' + last;
 }
 
 // Desafio 12
